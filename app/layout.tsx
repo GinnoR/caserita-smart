5965dcd0eafdc20a4a5abb9fde4fa91f80a85499
miguel20yt426-collab <miguel20yt426@gmail.com>
2026-06-26 21:46:08 -0500

    Feat: Add official bionic logo for PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,10 @@ export const metadata: Metadata = {
     statusBarStyle: "default",
     title: "Caserita Smart",
   },
+  icons: {
+    icon: "/logo.png",
+    apple: "/logo.png",
+  },
   formatDetection: {
     telephone: false,
   },
